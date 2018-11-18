5a27d2bb75c070d8fdbb4f2016a97e09c40d8f65
ian morton <ian.happytuesdays@gmail.com>
2018-11-18 19:42:11 +0000

    first save

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,57 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-navigation-drawer v-model="sideNav">
+      <v-list>
+       <v-list-tile v-for="item in menuItems" :key="item.title">
+         <v-list-tile-action>
+           <v-icon>
+             {{item.icon}}
+           </v-icon>
+         </v-list-tile-action>
+         <v-list-tile-content>{{item.title}}</v-list-tile-content>
+       </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app>
+      <v-toolbar-side-icon @click.native.stop="sideNav = !sideNav"
+      class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Max example</span>
+      </v-toolbar-title>
+       <v-spacer></v-spacer>
+       <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat v-for="item in menuItems" :key="item.title">
+          <v-icon left>{{item.icon}}</v-icon>
+          <span class="mr-2">{{item.title}}</span>
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+import HelloWorld from './components/HelloWorld'
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      sideNav: false,
+      menuItems: [
+        { icon: 'supervisor_account', title: 'View Listings'},
+        { icon: 'room', title: 'Organise Meetups'},
+        { icon: 'person', title: 'Profile'},
+        { icon: 'face', title: 'Sign Up'},
+        { icon: 'lock_open', title: 'Sign In'}
+      ]
+    }
+  }
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
